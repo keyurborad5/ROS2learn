a352784fecfa39c711e067dee08816fee78f145a
Keyur Borad <keyurborad5@gmail.com>
2023-12-05 16:08:25 -0500

    Added callback function to vehicle.cpp
    1. Added timer in vehicle.h
    2. Added callbackfuntion in vehicle.cpp
    3. added values to custom message in callback function.
    4. Run this particular executable and not the whole launch

--- rosprac809_ws/src/automated_vehicle/include/automated_vehicle/vehicle.h
+++ rosprac809_ws/src/automated_vehicle/include/automated_vehicle/vehicle.h
@@ -9,11 +9,15 @@ namespace automated_vehicle{
         //constructor
         Vehicle(std::string node_name): Node(node_name){
             vehicle_publisher_=this->create_publisher<av_msgs::msg::VehicleStatus>("vehicle_status",10);
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(100),std::bind(&automated_vehicle::Vehicle::vehicle_pub_data_cb,this));
 
         }
 
         private:
         rclcpp::Publisher<av_msgs::msg::VehicleStatus>::SharedPtr vehicle_publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        void vehicle_pub_data_cb();
 
     };
 }
--- rosprac809_ws/src/automated_vehicle/src/vehicle.cpp
+++ rosprac809_ws/src/automated_vehicle/src/vehicle.cpp
@@ -1,6 +1,15 @@
 #include"vehicle.h"
 #include<rclcpp/rclcpp.hpp>
 
+void automated_vehicle::Vehicle::vehicle_pub_data_cb(){
+    av_msgs::msg::VehicleStatus message;
+    message.driving_type= av_msgs::msg::VehicleStatus::AUTONOMOUS;
+    message.current_action= av_msgs::msg::VehicleStatus::STOPPED;
+    message.current_speed= 1.0;
+    vehicle_publisher_->publish(message);
+    RCLCPP_INFO_STREAM(this->get_logger(),"printing");
+}
+
 int main(int argc, char** argv){
     rclcpp::init(argc,argv);
     auto node = std::make_shared<automated_vehicle::Vehicle>("vehicle");
